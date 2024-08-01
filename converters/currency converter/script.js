27adc6391384e1083298602f812e82e3b14f1955
Aaditya-0406 <aadityashrivastava2004@gmail.com>
2024-08-01 21:20:47 +0530

    style added

--- converters/currency converter/script.js
+++ converters/currency converter/script.js
@@ -1,20 +1,41 @@
 const IND = document.getElementById("IND");
 const USD = document.getElementById("USD");
 const body = document.getElementById("body");
-const conversionRate = 83.70;
+let btn = document.getElementById("swap");
 
-body.addEventListener('keyup', () => {
-    if (document.activeElement === IND) {
-        const inrValue = parseFloat(IND.value);
-        if (!isNaN(inrValue)) {
-            const usdValue = inrValue / conversionRate;
-            USD.value = usdValue.toFixed(2);
-        }
-    } else if (document.activeElement === USD) {
-        const usdValue = parseFloat(USD.value);
-        if (!isNaN(usdValue)) {
-            const inrValue = usdValue * conversionRate;
-            IND.value = inrValue.toFixed(2);
-        }
-    } 
+const conversionRate = 83.7;
+let swap = true;
+btn.addEventListener("click", () => {
+  if (swap) {
+    body.style.transform = `rotate(${180}deg)`;
+    USD.style.transform = `rotate(${180}deg)`;
+    IND.style.transform = `rotate(${180}deg)`;
+    swap = false;
+  } else {
+    body.style.transform = `rotate(${360}deg)`;
+    USD.style.transform = `rotate(${360}deg)`;
+    IND.style.transform = `rotate(${360}deg)`;
+    swap = true;
+  }
 });
+
+function myEvent() {
+  if (document.activeElement.value === "") {
+    IND.value = "";
+    USD.value = "";
+  } else if (document.activeElement === IND) {
+    const inrValue = parseFloat(IND.value);
+    if (!isNaN(inrValue)) {
+      const usdValue = inrValue / conversionRate;
+      USD.value = usdValue.toFixed(3);
+    }
+  } else if (document.activeElement === USD) {
+    const usdValue = parseFloat(USD.value);
+    if (!isNaN(usdValue)) {
+      const inrValue = usdValue * conversionRate;
+      IND.value = inrValue.toFixed(3);
+    }
+  }
+}
+body.addEventListener("keyup", myEvent);
+body.addEventListener("click", myEvent);
