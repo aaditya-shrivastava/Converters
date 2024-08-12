fb027954577314aa75296483fb29940fb8695f66
Aaditya-0406 <aadityashrivastava2004@gmail.com>
2024-08-12 16:20:37 +0530

    new script

--- converters/currency converter/script.js
+++ converters/currency converter/script.js
@@ -1,41 +1,61 @@
-const IND = document.getElementById("IND");
-const USD = document.getElementById("USD");
-const body = document.getElementById("body");
-let btn = document.getElementById("swap");
+let BaseURL = `https://currency-converter18.p.rapidapi.com/api/v1/convert?`;
+const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "YOUR-API-KEY",
+    "x-rapidapi-host": "YOUR-HOST-NAME",
+  /* you can generate the api key from 
+  
+  https://rapidapi.com/solutionsbynotnull/api/currency-converter18/playground/apiendpoint_6660be9f-ceaf-49da-9557-4708c5a2ab62
+  
+  without any issue*/
+  },
+};
 
-const conversionRate = 83.7;
-let swap = true;
-btn.addEventListener("click", () => {
-  if (swap) {
-    body.style.transform = `rotate(${180}deg)`;
-    USD.style.transform = `rotate(${180}deg)`;
-    IND.style.transform = `rotate(${180}deg)`;
-    swap = false;
-  } else {
-    body.style.transform = `rotate(${360}deg)`;
-    USD.style.transform = `rotate(${360}deg)`;
-    IND.style.transform = `rotate(${360}deg)`;
-    swap = true;
-  }
-});
+let fromCurr = document.getElementById("from");
+let toCurr = document.getElementById("to");
+let dropDown = document.querySelectorAll(".selection select");
+let btn = document.getElementById("formBtn");
 
-function myEvent() {
-  if (document.activeElement.value === "") {
-    IND.value = "";
-    USD.value = "";
-  } else if (document.activeElement === IND) {
-    const inrValue = parseFloat(IND.value);
-    if (!isNaN(inrValue)) {
-      const usdValue = inrValue / conversionRate;
-      USD.value = usdValue.toFixed(3);
+for (let select of dropDown) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
     }
-  } else if (document.activeElement === USD) {
-    const usdValue = parseFloat(USD.value);
-    if (!isNaN(usdValue)) {
-      const inrValue = usdValue * conversionRate;
-      IND.value = inrValue.toFixed(3);
-    }
+    select.append(newOption);
   }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 }
-body.addEventListener("keyup", myEvent);
-body.addEventListener("click", myEvent);
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.getElementById("input");
+  let amountVal = amount.value;
+  if (amountVal <= 1 && amountVal === "") {
+    amountVal = 1;
+    amountVal.value = "1";
+  }
+
+  //! creating a BaseURL
+  let URL = `${BaseURL}from=${fromCurr.value}&to=${toCurr.value}&amount=${amountVal}`;
+  let response = await fetch(URL, options);
+  let data = await response.json();
+  let finalAmount = data.result.convertedAmount;
+  document.getElementById(
+    "message"
+  ).innerText = `${amountVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+});
